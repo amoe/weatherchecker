6cc54697d5979d3406ba13fd3a4ffe475d25d656
David Banks <amoebae@gmail.com>
2021-10-07 14:22:03 +0100

    no magic numbers

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,5 +1,6 @@
 import pyowm
 
+PEACE_ANGEL_COORDINATES = (50.82257, -0.15690)
 
 def test_foo():
     with open('.owm_key', 'r') as f:
@@ -8,5 +9,5 @@ def test_foo():
 
     owm = pyowm.OWM(key)
     mgr = owm.weather_manager()
-    observation = mgr.weather_at_coords(50.82257, -0.15690)
+    observation = mgr.weather_at_coords(*PEACE_ANGEL_COORDINATES)
     assert 2 + 2 == 4
