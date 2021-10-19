19e1a369af3ea296e04a07b3b89f0023d1a487a5
David Banks <amoebae@gmail.com>
2021-10-19 14:02:36 +0100

    forecast testing

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,10 +1,16 @@
 import pyowm
 import pdb
+import datetime
 
 PEACE_ANGEL_COORDINATES = (50.82257, -0.15690)
 
 def feels_like(weather):
     return weather.temperature('celsius')['feels_like']
+
+def time_from_epoch_time(epoch_time):
+    timestamp = datetime.datetime.utcfromtimestamp(epoch_time)
+    return timestamp.time()
+    
 
 def test_foo():
     with open('.owm_key', 'r') as f:
@@ -41,13 +47,13 @@ def test_foo():
     print("Visibility is:", weather.visibility())
 
     # If a gust hits "strong breeze", complain
-    gust = wind_mph['gust']
+    wind_speed = wind_mph['speed']
 
-    if gust >= 25:
+    if wind_speed >= 25:
         print("It's much too windy for a run.")
-    elif gust >= 13:
+    elif wind_speed >= 13:
         print("You're gonna be against the wind, at least one way.")
-    elif gust >= 3:
+    elif wind_speed >= 3:
         print("There's some wind, but not enough to care about.")
     else:
         print("There's basically no wind.")
@@ -60,8 +66,9 @@ def test_foo():
         print("It's cold, better wear a jumper.")
 
 
-    # TODO parse out sunset time
-    # print(weather.sunset_time())
+    # TODO parse out sunset time and correlate to forecasts
+    sunrise_time = time_from_epoch_time(weather.sunrise_time())
+    sunset_time = time_from_epoch_time(weather.sunset_time())
         
     forecaster = mgr.forecast_at_coords(*PEACE_ANGEL_COORDINATES, interval='3h')
 
@@ -70,9 +77,12 @@ def test_foo():
     for weather in forecaster.forecast:
         wind = weather.wind(unit='miles_hour')
         rain = weather.rain
-        print(weather.to_dict())
-        if wind['gust'] < 25 and not rain and weather.clouds <= 50:
-            print(weather.reference_time('iso'), weather.detailed_status, weather.clouds, weather.sunset_time(timeformat='unix'))
+        
+        this_ts = datetime.datetime.utcfromtimestamp(weather.reference_time())
+        this_time = this_ts.time()
+#        print(weather.to_dict())
+        if wind['speed'] < 25 and not rain and weather.clouds <= 50 and this_time > sunrise_time and this_time < sunset_time:
+            print(weather.reference_time('iso'), weather.detailed_status, weather.clouds)
             
 
     # clear_intervals = forecaster.when_clear()
