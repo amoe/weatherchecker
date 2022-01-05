326723bd7146a91ecfdb0164badc00bf5972350a
David Banks <amoebae@gmail.com>
2022-01-05 07:30:55 +0000

    factoring out engine

--- weatherchecker/views.py
+++ weatherchecker/views.py
@@ -1,9 +1,15 @@
 from pyramid.view import view_config
 import pyramid.response
 
-@view_config(
-    route_name='home',
-    renderer='weatherchecker:templates/home.j2'
-)
+# Not using templates at the moment
+#
+# @view_config(
+#     route_name='home',
+#     renderer='weatherchecker:templates/home.j2'
+# )
+# def my_view(request):
+#     return {'project': 'myproject4'}
+
+@view_config(route_name='home')
 def my_view(request):
-    return {'project': 'myproject4'}
+    return pyramid.response.Response("Foo bar baz", content_type='text/plain')
